b28b9d33f056a17a82c12386d11cb6697380a51f
Chris C <casson@purduecal.edu>
2017-01-04 17:24:53 -0600

    Add NYT articles.

--- js/script.js
+++ js/script.js
@@ -20,6 +20,28 @@ function loadData() {
     
     var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     $body.append('<img class="bgimg" src="' + streetviewURL + '">');
+    
+    var nytParameters = {
+        "q" : city,
+        "sort" : "newest",
+        "fl" : "web_url,snippet,headline",
+        "api-key" : "8521f06c7ebd4c3693837b75c8f767e4"
+    };
+    
+    $.getJSON("https://api.nytimes.com/svc/search/v2/articlesearch.json", nytParameters, function(data) {
+        $nytHeaderElem.text("New York Times Articles About " + city);
+        
+        $.each(data.response.docs, function(key, doc) {
+            $nytElem.append(
+                    "<li class='article'>" + 
+                        "<a href='" + doc.web_url + "'>" + doc.headline.main + "</a>" + 
+                        "<p>" + doc.snippet + "</p>" + 
+                    "</li>"
+                    );
+        });
+    }).fail(function() {
+        $nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
+    });
 
     return false;
 };
